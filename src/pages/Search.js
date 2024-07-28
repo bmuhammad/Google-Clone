dd43bb0c2ced07d20f9a780890a218b6e82bbf53
Brian <briancmuhammad@yahoo.com>
2024-07-28 16:00:50 -0400

    added navigation bar, search bar with buttons below

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Search.css";
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
+import { Button } from "@mui/material";
 
 function Search() {
   return (
-    <div>
-   <h1>This is search page</h1>
+    <div className="search">
+  <div className="search__input">
+    
+    <SearchIcon className="search__inputIcon" />
+    <input />
+   <MicIcon />
+  </div>
+  <div className="search__buttons">
+    <Button variant="outlined">Google Search</Button>
+    <Button variant="outlined">I'm Feeling Lucky</Button>
+  </div>
     </div>
   );
 }
